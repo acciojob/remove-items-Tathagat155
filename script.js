0e89de5634b4a70865c19c13118abe9ba2c7fe72
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-25 13:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+    const select = document.getElementById("colorSelect");
+
+    // find selected index
+    const index = select.selectedIndex;
+
+    if (index !== -1) {
+        select.remove(index);   // remove the selected option
+    }
+}
